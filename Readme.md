65aa9729c63df0cd259fcccbd5cbbb2b9977f005
zhujiangtao <jarvischu@126.com>
2021-09-16 21:32:46 +0800

    update readme

--- Readme.md
+++ Readme.md
@@ -1,5 +1,48 @@
 # Simple WebSocket Client
 
-Based on https://github.com/dhbaird/easywsclient
+**ONLY One header file** implementation of Web Socket Cliet.
 
-> [WebSocket协议：5分钟从入门到精通](https://www.cnblogs.com/chyingp/p/websocket-deep-in.html)
+> Based on https://github.com/dhbaird/easywsclient
+
+## Example
+
+```cpp
+#include "sws.hpp" // all you have to include is this header file
+#include <iostream>
+
+class WebSocketCB : public sws::IWebSocketCB {
+public:
+    void OnRecvMessage(sws::OpCodeType opcode, const std::string& msg) {
+        if (opcode == sws::TEXT_FRAME) {
+            std::cout << "Receive Text: " << msg << std::endl;
+        }else if (opcode == sws::BINARY_FRAME) {
+            std::cout << "Receive Binary, size:" << msg.size() << std::endl;
+        }
+    }
+
+    void OnDisconnected(const std::string& msg) {}
+};
+
+int main()
+{
+    WebSocketCB cb;
+    sws::WebSocketClient client;
+    bool ret = client.Connect("ws://echo.websocket.org", &cb);
+    if (!ret) {
+        printf("connect failed, err:%s\n", client.GetLastError().c_str());
+        return 1;
+    }
+
+    while (true) {
+        std::string input;
+        std::cout << "Enter message to send: ";
+        std::getline(std::cin, input);
+        if (input == "quit") break;
+        else {
+            client.SendTextMessage(input);
+        }
+    }
+
+    return 0;
+}
+```
--- example.cpp
+++ example.cpp
@@ -3,39 +3,39 @@
 
 class WebSocketCB : public sws::IWebSocketCB {
 public:
-	void OnRecvMessage(sws::OpCodeType opcode, const std::string& msg) {
-		if (opcode == sws::TEXT_FRAME) {
-			std::cout << "Receive Text: " << msg << std::endl;
-		}
-		else if (opcode == sws::BINARY_FRAME) {
-			std::cout << "Receive Binary, size:" << msg.size() << std::endl;
-		}
-	}
+    void OnRecvMessage(sws::OpCodeType opcode, const std::string& msg) {
+        if (opcode == sws::TEXT_FRAME) {
+            std::cout << "Receive Text: " << msg << std::endl;
+        }
+        else if (opcode == sws::BINARY_FRAME) {
+            std::cout << "Receive Binary, size:" << msg.size() << std::endl;
+        }
+    }
 
-	void OnDisconnected(const std::string& msg) {
-		std::cout << "OnDisconnected: " << msg << std::endl;
-	}
+    void OnDisconnected(const std::string& msg) {
+        std::cout << "OnDisconnected: " << msg << std::endl;
+    }
 };
 
 int main()
 {
-	WebSocketCB cb;
-	sws::WebSocketClient client;
-	bool ret = client.Connect("ws://echo.websocket.org", &cb);
-	if (!ret) {
-		printf("connect failed, err:%s\n", client.GetLastError().c_str());
-		return 1;
-	}
+    WebSocketCB cb;
+    sws::WebSocketClient client;
+    bool ret = client.Connect("ws://echo.websocket.org", &cb);
+    if (!ret) {
+        printf("connect failed, err:%s\n", client.GetLastError().c_str());
+        return 1;
+    }
 
-	while (true) {
-		std::string input;
-		std::cout << "Enter message to send: ";
-		std::getline(std::cin, input);
-		if (input == "quit") break;
-		else {
-			client.SendTextMessage(input);
-		}
-	}
+    while (true) {
+        std::string input;
+        std::cout << "Enter message to send: ";
+        std::getline(std::cin, input);
+        if (input == "quit") break;
+        else {
+            client.SendTextMessage(input);
+        }
+    }
 
-	return 0;
+    return 0;
 }
